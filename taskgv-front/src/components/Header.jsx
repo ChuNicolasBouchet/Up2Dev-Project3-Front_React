973ed6885beb27c2410a77e54adde0492e19d945
Nicolas Bouchet <chunico@pm.me>
2022-06-15 15:30:29 +0200

    header css fixed

--- taskgv-front/src/components/Header.jsx
+++ taskgv-front/src/components/Header.jsx
@@ -8,7 +8,13 @@ import React from 'react'
 function Header() {
   return (
     <div className='header__box'>
-      SNCF logo - header
+      <div className='placeholder__burger'></div>
+      <div className='header__logo_sncf'>
+        <img src={require('../assets/sncf-logo.png')} alt='sncf entreprise logo' />
+      </div>
+      <div className='header__site-name'>T@skGV</div>
+      <div className='header__wrapper__growing'></div>
+      <div className='header__welcome-user'>Bienvenue Marcel</div>
     </div>
   )
 }

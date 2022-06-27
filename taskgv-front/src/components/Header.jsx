a81026a7788b5c5392447ed4492b6bf1ba0f6fd5
Nicolas Bouchet <chunico@pm.me>
2022-06-27 21:22:12 +0200

    fixed burger menu style

--- taskgv-front/src/components/Header.jsx
+++ taskgv-front/src/components/Header.jsx
@@ -16,12 +16,12 @@ function Header() {
       {/* <div className='placeholder__burger'></div> */}
       <PopMenu menuItems={toolBarItems} menuClass='burger' className='burger-component' />
       <div className='header__logo__wrapper'>
-      <img className='header__logo__sncf' src={SncfLogo} alt='sncf logo' />
+        <img className='header__logo__sncf' src={SncfLogo} alt='sncf logo' />
       </div>
       <LogoTaskgv className='header__logo__taskgv'/>
       <div className='header__wrapper__growing'></div>
       <div className='header__user-connect'>
-      { auth.userInfos ? <div className='header__welcome-user'>Bienvenue {auth.userInfos.userEmail} <Logout /></div> : <Link className='header__welcome-user' to='/login'>Connectez-vous</Link> }
+        { auth.userInfos ? <div className='header__welcome-user'>Bienvenue {auth.userInfos.userEmail} <Logout /></div> : <Link className='header__welcome-user' to='/login'>Connectez-vous</Link> }
       </div>
     </div>
   )

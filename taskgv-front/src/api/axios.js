4879390aa3b624046f7db0da017d196510da0daf
Nicolas Bouchet <chunico@pm.me>
2022-06-28 10:41:42 +0200

    dot env added and api/axios configured

--- taskgv-front/src/api/axios.js
+++ taskgv-front/src/api/axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:5000/api';
+// const BASE_URL = 'http://localhost:5000/api';
 
 export default axios.create({
-    baseURL: BASE_URL
+    baseURL: process.env.BASE_URL
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
+    baseURL: process.env.BASE_URL,
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 });
